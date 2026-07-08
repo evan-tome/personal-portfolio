7cd5cafae5d05d482943702f285873f03d5219fa
EvTome <evtome05@gmail.com>
2026-07-08 00:14:50 -0400

    Fixed some leftover migration issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Skills from './components/Skills';
+
+import './App.css';
 
 function App() {
   return (
